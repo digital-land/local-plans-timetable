50614475192fe151af813cb95f57b540b630ccb2
vferraro-scottlogic <vferraro@scottlogic.com>
2024-01-29 16:02:31 +0000

    SOW-24: Add Story for Form

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { withoutVitePlugins } from "@storybook/builder-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -14,6 +15,14 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, [
+        "vite:lib-inject-css",
+      ]),
+    };
   },
 };
 export default config;
--- lib/stories/Form.stories.ts
+++ lib/stories/Form.stories.ts
@@ -1,27 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Header } from './Header';
+import { Form } from '../timetable-form/Form';
 
 const meta = {
-  title: 'Example/Header',
-  component: Header,
+  title: 'Example/Form',
+  component: Form,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Header>;
+} satisfies Meta<typeof Form>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
-};
-
-export const LoggedOut: Story = {};
+export const ExampleForm: Story = {};
