6c84512e6fe8a16e492d58102565061ffaf251e2
osouthwell-scottlogic <osouthwell@scottlogic.com>
2024-01-30 11:30:05 +0000

    SOW-24: remove extra mocking file

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,10 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-        "^.+\\.tsx?$": "ts-jest" 
-    },
-    moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4)$":
-          "<rootDir>/tests/mocks/fileMock.js"
-   },
-  }
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
