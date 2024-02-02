661a3a2d7d1bc5fdbab750a0591606044ec242cd
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-02 09:04:41 +0000

    SOW-24: add snapshots to unit tests

--- lib/gds-components/date-input/DateInput.test.tsx
+++ lib/gds-components/date-input/DateInput.test.tsx
@@ -29,28 +29,37 @@ const DateInputWrapper = ({
 };
 
 it("shows the right input values from the provided value", () => {
-  render(<DateInputWrapper initialValue="3-2007" onChange={() => {}} />);
+  const { asFragment } = render(
+    <DateInputWrapper initialValue="3-2007" onChange={() => {}} />
+  );
 
+  expect(asFragment()).toMatchSnapshot();
   expect(screen.getByLabelText("Month")).toHaveValue("3");
   expect(screen.getByLabelText("Year")).toHaveValue("2007");
 });
 
 it("calls the onChange handler with the right value when the month input changes", async () => {
   const onChange = jest.fn();
-  render(<DateInputWrapper initialValue="3-2007" onChange={onChange} />);
+  const { asFragment } = render(
+    <DateInputWrapper initialValue="3-2007" onChange={onChange} />
+  );
 
   await userEvent.clear(screen.getByLabelText("Month"));
   await userEvent.type(screen.getByLabelText("Month"), "4");
 
+  expect(asFragment()).toMatchSnapshot();
   expect(onChange).toHaveBeenLastCalledWith("4-2007");
 });
 
 it("calls the onChange handler with the right value when the year input changes", async () => {
   const onChange = jest.fn();
-  render(<DateInputWrapper initialValue="3-2007" onChange={onChange} />);
+  const { asFragment } = render(
+    <DateInputWrapper initialValue="3-2007" onChange={onChange} />
+  );
 
   await userEvent.clear(screen.getByLabelText("Year"));
   await userEvent.type(screen.getByLabelText("Year"), "2008");
 
+  expect(asFragment()).toMatchSnapshot();
   expect(onChange).toHaveBeenLastCalledWith("3-2008");
 });
--- lib/gds-components/text-input/TextInput.test.tsx
+++ lib/gds-components/text-input/TextInput.test.tsx
@@ -5,9 +5,12 @@ import { TextInput } from "./TextInput";
 
 it("returns the plain text value", async () => {
   const onChange = jest.fn();
-  render(<TextInput label="input" onChange={onChange} value="" />);
+  const { asFragment } = render(
+    <TextInput label="input" onChange={onChange} value="" />
+  );
 
   await userEvent.type(screen.getByLabelText("input"), "a");
 
+  expect(asFragment()).toMatchSnapshot();
   expect(onChange).toHaveBeenCalledWith("a");
 });
