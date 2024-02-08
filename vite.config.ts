81b1218080b8ecc2e362be06a374fa988d4f28ae
vferraro-scottlogic <vferraro@scottlogic.com>
2024-02-08 16:41:23 +0000

    SOW-24: change entry file folder in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "local-plans-timetable/form.js",
+        assetFileNames: "local-plans-timetable/form.css",
+      },
+    },
+  },
 });
