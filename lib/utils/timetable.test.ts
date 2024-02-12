d9dbca5f91f799fb94db9678439150665122d374
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-12 15:42:13 +0000

    SOW-24: add test case for resolveTimetableEventsCSV

--- lib/utils/timetable.test.ts
+++ lib/utils/timetable.test.ts
@@ -180,4 +180,98 @@ describe("resolveTimetableEventsCSV", () => {
         `3,name,development-plan,examination-hearing-start,2023-01-02,,organisation,${currentDate},${currentDate},`
     );
   });
+
+  test("returns the correct CSV string if there are changes and previously invalidated rows", () => {
+    (uuidv4 as unknown as jest.SpyInstance).mockImplementation(() => "4");
+    const timetableEvents: DevelopmentPlanTimetable[] = [
+      {
+        reference: "1",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "public-consultation-start",
+        eventDate: "2023-01-03",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2022-12-20",
+        startDate: "2022-12-20",
+        endDate: "2023-01-01",
+      },
+      {
+        reference: "2",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "public-consultation-start",
+        eventDate: "2023-01-01",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2023-01-01",
+        startDate: "2023-01-01",
+        endDate: "",
+      },
+      {
+        reference: "3",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "examination-hearing-start",
+        eventDate: "2023-01-02",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2023-01-01",
+        startDate: "2023-01-01",
+        endDate: "",
+      },
+    ];
+    const loadedTimetableEvents: DevelopmentPlanTimetable[] = [
+      {
+        reference: "1",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "public-consultation-start",
+        eventDate: "2023-01-03",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2022-12-20",
+        startDate: "2022-12-20",
+        endDate: "2023-01-01",
+      },
+      {
+        reference: "2",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "public-consultation-start",
+        eventDate: "2023-01-01",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2023-01-01",
+        startDate: "2023-01-01",
+        endDate: "",
+      },
+      {
+        reference: "3",
+        name: "name",
+        developmentPlan: "development-plan",
+        developmentPlanEvent: "examination-hearing-start",
+        eventDate: "2023-01-03",
+        notes: "",
+        organisation: "organisation",
+        entryDate: "2023-01-01",
+        startDate: "2023-01-01",
+        endDate: "",
+      },
+    ];
+
+    const result = resolveTimetableEventsCSV(
+      timetableEvents,
+      loadedTimetableEvents
+    );
+
+    const currentDate = getFormattedDate();
+    expect(result).toBe(
+      "reference,name,developmentPlan,developmentPlanEvent,eventDate,notes,organisation,entryDate,startDate,endDate\n" +
+        "1,name,development-plan,public-consultation-start,2023-01-03,,organisation,2022-12-20,2022-12-20,2023-01-01\n" +
+        "2,name,development-plan,public-consultation-start,2023-01-01,,organisation,2023-01-01,2023-01-01,\n" +
+        `3,name,development-plan,examination-hearing-start,2023-01-03,,organisation,2023-01-01,2023-01-01,${currentDate}\n` +
+        `4,name,development-plan,examination-hearing-start,2023-01-02,,organisation,${currentDate},${currentDate},`
+    );
+  });
 });
