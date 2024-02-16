41b010d28b46667be68ffe59d2e803940763bb13
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-16 17:00:10 +0000

    SOW-24: cleanup test console errors and warnings

--- lib/timetable-visualisation/Visualisation.test.tsx
+++ lib/timetable-visualisation/Visualisation.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { loadCSV } from "../utils/timetable";
 import { Visualisation } from "./Visualisation";
 import csvToJson from "csvtojson";
@@ -19,45 +19,53 @@ const headersFilepath = "/header";
 describe("Visualisation", () => {
   beforeEach(() => {
     (loadCSV as jest.Mock).mockResolvedValue("");
-    (PlanViewer as jest.Mock).mockReturnValue(<></>);
+    (PlanViewer as jest.Mock).mockReturnValue(<>Plan viewer component</>);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("renders", () => {
-    render(
-      <Visualisation
-        stagesFilepath={stagesFilePath}
-        headersFilepath={headersFilepath}
-      />
-    );
+  it("renders", async () => {
+    await act(async () => {
+      render(
+        <Visualisation
+          stagesFilepath={stagesFilePath}
+          headersFilepath={headersFilepath}
+        />
+      );
+    });
+
     expect(screen.getByTestId("visualisation")).toBeInTheDocument();
   });
 
   it("calls loadCSV and csvToJson twice on mount", async () => {
-    render(
-      <Visualisation
-        stagesFilepath={stagesFilePath}
-        headersFilepath={headersFilepath}
-      />
-    );
-    await waitFor(() => {
-      expect(loadCSV).toHaveBeenCalledTimes(2);
-      expect(loadCSV).toHaveBeenNthCalledWith(1, stagesFilePath);
-      expect(loadCSV).toHaveBeenNthCalledWith(2, headersFilepath);
-
-      expect(csvToJson).toHaveBeenCalledTimes(2);
+    await act(async () => {
+      render(
+        <Visualisation
+          stagesFilepath={stagesFilePath}
+          headersFilepath={headersFilepath}
+        />
+      );
     });
+
+    expect(loadCSV).toHaveBeenCalledTimes(2);
+    expect(loadCSV).toHaveBeenNthCalledWith(1, stagesFilePath);
+    expect(loadCSV).toHaveBeenNthCalledWith(2, headersFilepath);
+
+    expect(csvToJson).toHaveBeenCalledTimes(2);
   });
-  it("renders a PlanPreview component", () => {
-    render(
-      <Visualisation
-        stagesFilepath={stagesFilePath}
-        headersFilepath={headersFilepath}
-      />
-    );
-    expect(PlanViewer).toHaveBeenCalledTimes(1);
+
+  it("renders a PlanViewer component", async () => {
+    await act(async () => {
+      render(
+        <Visualisation
+          stagesFilepath={stagesFilePath}
+          headersFilepath={headersFilepath}
+        />
+      );
+    });
+
+    expect(screen.getByText("Plan viewer component")).toBeInTheDocument();
   });
 });
