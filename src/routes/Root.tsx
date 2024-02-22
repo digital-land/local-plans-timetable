46c472e1b7ff58d9724a503bb514c29d870bdfff
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-22 13:50:10 +0000

    SOW-24: add @lib alias

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,6 @@
-import { Form } from "../../lib/main";
+import { Form } from "@lib/main";
 
 // The root will eventually be the starting page of the form (create or update)
-
 export const Root = () => {
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/local-plans-timetable/",
+  resolve: {
+    alias: {
+      "@lib": path.resolve(__dirname, "lib"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
