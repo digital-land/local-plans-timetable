7ca9de0e0a8e62873df07f48538314213aef069d
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-22 13:34:53 +0000

    SOW-24: add basic react-router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Root } from "./routes/Root";
+
+const router = createBrowserRouter([{ path: "/", element: <Root /> }], {
+  basename: "/local-plans-timetable",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/routes/Root.tsx
@@ -0,0 +1,11 @@
+import { Form } from "../../lib/main";
+
+// The root will eventually be the starting page of the form (create or update)
+
+export const Root = () => {
+  return (
+    <>
+      <Form />
+    </>
+  );
+};
