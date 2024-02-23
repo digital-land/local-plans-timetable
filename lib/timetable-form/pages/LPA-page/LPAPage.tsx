09c3da07692c7aa167e65edc961e91e4f37eade0
osouthwell-scottlogic <osouthwell@scottlogic.com>
2024-02-23 10:27:02 +0000

    SOW-24: readd removed back button

--- lib/timetable-form/pages/LPA-page/LPAPage.tsx
+++ lib/timetable-form/pages/LPA-page/LPAPage.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { fetchLPAs } from "../../../api";
-import { Button } from "../../../gds-components";
-import { Autocomplete } from "../../autocomplete/Autocomplete";
+import { fetchLPAs } from "../../api";
+import { Button } from "../../gds-components";
+import { Autocomplete } from "../autocomplete/Autocomplete";
 
-import styles from "../../styles.module.css";
+import styles from "../styles.module.css";
 
 export const LPAPage = (): JSX.Element => {
   const [LPAs, setLPAs] = useState<string[]>([]);
@@ -25,6 +25,9 @@ export const LPAPage = (): JSX.Element => {
   return (
     <div>
       <div className={styles.form}>
+        <a href="#" className="govuk-back-link">
+          Back
+        </a>
         <h1 className="govuk-heading-l govuk-!-margin-top-6">
           What is your Local Authority?
           <span className="govuk-caption-m govuk-!-margin-top-3">
