04e21d1acf453ccc2d4744e792dd9d023d5b3db2
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-26 16:32:57 +0000

    SOW-24: add basic export page

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import { validateTimetableStage } from "./pages/stage-page/validate-stage-page";
 import { validateTitle } from "./pages/title-page/validate-title-page";
 import { validatePublishLDSEvent } from "./pages/publish-LDS-page/validate-publish-LDS-page";
 import { CreateTimetablePage } from "./pages/create-timetable-page/CreateTimetablePage";
+import { ExportPage } from "./pages/export-page/ExportPage";
 import { stages } from "./pages/stages";
 import { Page } from "./routes/Page";
 import { PageRoute } from "./routes/routes";
@@ -56,6 +57,10 @@ const router = createBrowserRouter(
           path: key,
           element: FormPageHoC(StagePage, otherProps, validateTimetableStage),
         })),
+        {
+          path: PageRoute.Export,
+          element: FormPageHoC(ExportPage, {}),
+        },
       ],
     },
   ],
