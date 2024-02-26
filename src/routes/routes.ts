db9354d73a7e0a1a070006b34af2144daf16fe0e
Ro4052 <Ro4052@users.noreply.github.com>
2024-02-26 10:12:52 +0000

    SOW-24: align existing routes to design

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,10 @@
 export enum PageRoute {
   Base = "/local-plans-timetable",
   Root = "/",
-  LPA = "/lpa",
-  Title = "/title",
-  Description = "/description",
+  LPA = "/what-is-your-local-authority",
+  Title = "/title-of-local-plan",
+  Description = "/local-plan-description",
+  PublicConsultation = "/public-consultation-stage",
   Submission = "/submission",
-  PublicConsultation = "/public-consultation",
   // add all pages
 }
