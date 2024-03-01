168ad72c68625c0d86817d710d40ef3b4be939ea
osouthwell-scottlogic <osouthwell@scottlogic.com>
2024-03-01 14:05:54 +0000

    SOW-24: scroll to top of new page

--- src/routes/Page.tsx
+++ src/routes/Page.tsx
@@ -3,9 +3,11 @@ import { Outlet } from "react-router";
 import { Header, Footer } from "@lib/gds-components";
 import { PageRoute } from "./routes";
 import { FormProvider } from "../context/FormContext";
+import ScrollToTop from "./ScrollToTop";
 
 export const Page = () => (
   <>
+    <ScrollToTop />
     <Header homeURL={PageRoute.Base} />
     <FormProvider>
       <div className="govuk-width-container app-width-container">
