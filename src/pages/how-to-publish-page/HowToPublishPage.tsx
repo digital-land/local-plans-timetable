b13879156fb61888fbc740425b125d36611fd42f
Ro4052 <Ro4052@users.noreply.github.com>
2024-03-05 11:33:46 +0000

    SOW-24: separate nav into wrapper component

--- src/pages/how-to-publish-page/HowToPublishPage.tsx
+++ src/pages/how-to-publish-page/HowToPublishPage.tsx
@@ -1,2 +1,7 @@
-export const HowToPublishPage = () =>
-  "How to publish a Local Plan timetable online";
+import { NavPageWrapper } from "../nav-page-wrapper/NavPageWrapper";
+
+export const HowToPublishPage = () => (
+  <NavPageWrapper pageTitle="How to publish a Local Plan timetable online">
+    Page content
+  </NavPageWrapper>
+);
