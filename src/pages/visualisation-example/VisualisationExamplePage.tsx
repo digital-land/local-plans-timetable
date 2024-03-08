7954f2d563d290b13d61fad6b42914e0a79de6a7
vferraro-scottlogic <vferraro@scottlogic.com>
2024-03-08 14:54:38 +0000

    SOW-24: point to raw file from GH

--- src/pages/visualisation-example/VisualisationExamplePage.tsx
+++ src/pages/visualisation-example/VisualisationExamplePage.tsx
@@ -3,8 +3,8 @@ import { Visualisation } from "@lib/main";
 export const VisualisationExamplePage = (): JSX.Element => {
   return (
     <Visualisation
-      developmentPlanFilepath={"assets/development-plan.csv"}
-      timetableEventsFilepath={"assets/timetable.csv"}
+      developmentPlanFilepath={"https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/development-plan.csv"}
+      timetableEventsFilepath={"https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/timetable.csv"}
     />
   );
 };
