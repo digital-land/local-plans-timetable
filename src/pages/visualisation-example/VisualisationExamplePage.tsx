cec08d69536b096f4cede04a475bbb1c97b8eca8
vferraro-scottlogic <vferraro@scottlogic.com>
2024-03-11 11:15:30 +0000

    SOW-24: rename route

--- src/pages/visualisation-example/VisualisationExamplePage.tsx
+++ src/pages/visualisation-example/VisualisationExamplePage.tsx
@@ -5,7 +5,9 @@ export const VisualisationExamplePage = (): JSX.Element => {
   return (
     <NavPageWrapper pageTitle="What a Local Plan timetable created online looks like">
       <p className="govuk-body">
-      This is an example of what a timetable created using the online form will look like on a webpage. 
+        This is an{" "}
+        <strong className={`govuk-!-font-weight-bold`}>example</strong> of what
+        a timetable created using the online form will look like on a webpage.
       </p>
       <Visualisation
         developmentPlanFilepath={
