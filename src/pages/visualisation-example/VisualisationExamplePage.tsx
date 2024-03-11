95ab5b4659765b221e8de670d721d6b8de5d48b7
vferraro-scottlogic <vferraro@scottlogic.com>
2024-03-11 11:02:16 +0000

    SOW-24 add link to visualisation example

--- src/pages/visualisation-example/VisualisationExamplePage.tsx
+++ src/pages/visualisation-example/VisualisationExamplePage.tsx
@@ -1,10 +1,20 @@
 import { Visualisation } from "@lib/main";
+import { NavPageWrapper } from "../nav-page-wrapper/NavPageWrapper";
 
 export const VisualisationExamplePage = (): JSX.Element => {
   return (
-    <Visualisation
-      developmentPlanFilepath={"https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/development-plan.csv"}
-      timetableEventsFilepath={"https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/timetable.csv"}
-    />
+    <NavPageWrapper pageTitle="What a Local Plan timetable created online looks like">
+      <p className="govuk-body">
+      This is an example of what a timetable created using the online form will look like on a webpage. 
+      </p>
+      <Visualisation
+        developmentPlanFilepath={
+          "https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/development-plan.csv"
+        }
+        timetableEventsFilepath={
+          "https://raw.githubusercontent.com/digital-land/local-plans-timetable/main/assets/timetable.csv"
+        }
+      />
+    </NavPageWrapper>
   );
 };
