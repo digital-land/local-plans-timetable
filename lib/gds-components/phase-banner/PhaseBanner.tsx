6209bc911732a8ee7d4e8d6387ac8ad65189f92b
vferraro-scottlogic <vferraro@scottlogic.com>
2024-03-12 11:38:52 +0000

    SOW-24: show link text without link

--- lib/gds-components/phase-banner/PhaseBanner.tsx
+++ lib/gds-components/phase-banner/PhaseBanner.tsx
@@ -2,11 +2,7 @@
  * GDS component: https://design-system.service.gov.uk/components/phase-banner/
  */
 
-type PhaseBannerProps = {
-  linkUrl?: string;
-};
-
-export const PhaseBanner = ({ linkUrl }: PhaseBannerProps) => {
+export const PhaseBanner = () => {
   return (
     <>
       <div className="govuk-phase-banner">
@@ -15,17 +11,12 @@ export const PhaseBanner = ({ linkUrl }: PhaseBannerProps) => {
             Beta
           </strong>
           <span className="govuk-phase-banner__text">
-            This is a new service
+            This is a new service – your{" "}
+            <a className="govuk-link" href="/">
+              feedback
+            </a>{" "}
+            will help us to improve it.
           </span>
-          {linkUrl && (
-            <span>
-              – your{" "}
-              <a className="govuk-link" href={linkUrl}>
-                feedback
-              </a>{" "}
-              will help us to improve it.
-            </span>
-          )}
         </p>
       </div>
     </>
