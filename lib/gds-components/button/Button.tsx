11b66d9ba656d12dc1b73a3f216341da7f83f3da
Vasco <98337074+vferraro-scottlogic@users.noreply.github.com>
2024-03-21 08:49:31 +0000

    SOW-24: add text to copy button (#123)
    * SOW-24: add text to copy button
    
    * SOW-24: fix padding
    
    * SOW-24: use kebab-case for css classes

--- lib/gds-components/button/Button.tsx
+++ lib/gds-components/button/Button.tsx
@@ -2,14 +2,17 @@
  * GDS component: https://design-system.service.gov.uk/components/button/
  */
 
+import cn from "classnames";
+
 interface ButtonProps {
   children?: React.ReactNode;
   type?: "submit" | "button" | "reset";
   onClick?: () => void;
+  className?: string
 }
 
-export const Button = ({ children, type = "button", onClick }: ButtonProps) => (
-  <button type={type} className="govuk-button" onClick={onClick}>
+export const Button = ({ children, type = "button", onClick, className }: ButtonProps) => (
+  <button type={type} className={cn("govuk-button", className)} onClick={onClick}>
     {children}
   </button>
 );
