2f4cc0bc59ed0e21ff5e4d72032f261ac8ce66ce
Vasco <98337074+vferraro-scottlogic@users.noreply.github.com>
2024-03-22 14:14:10 +0000

    SOW-24: add details to description page (#129)
    * SOW-24: add details to description page
    
    * SOW-24: move 2/3 width to page
    
    * SOW-24: don't wrap Export page in FormPageHOC

--- src/pages/publish-LDS-page/PublishLDSPage.tsx
+++ src/pages/publish-LDS-page/PublishLDSPage.tsx
@@ -25,7 +25,7 @@ export const PublishLDSPage = ({
 
   return (
     <>
-      <h1 className="govuk-heading-l govuk-!-margin-top-6  govuk-!-width-two-thirds">
+      <h1 className="govuk-heading-l govuk-!-margin-top-6">
         Local development scheme (LDS) publish date
         <span className="govuk-caption-m govuk-!-margin-top-3">
           This should also be the date that you publish your timetable on your
